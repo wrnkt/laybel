32ba69fe37f5e10283bcf758dbc1c0dd846b2be3
Niko <wrnkt1@gmail.com>
2022-10-24 00:43:46 -0400

    Fix type of listing_url

--- trackpricebay.py
+++ trackpricebay.py
@@ -6,7 +6,7 @@ def create_parser():
     parser = argparse.ArgumentParser(description='')
     parser.add_argument("interval",type=int, help="The number of times to pull data per
             day. 1 or an even number between 2 and 24.")
-    parser.add_argument("listing_url", type=Path, help='The ebay listing url.')
+    parser.add_argument("listing_url", type=url, help='The ebay listing url.')
     return parser
 
 parser = argparse.ArgumentParser()
