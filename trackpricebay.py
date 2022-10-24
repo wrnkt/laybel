3e891526472ef7aa62a1b1f42c85446fe2b17b9b
Niko <wrnkt1@gmail.com>
2022-10-23 23:23:29 -0400

    Basic structure and getting inputs

---
+++ trackpricebay.py
@@ -0,0 +1,17 @@
+import sys
+import argparse
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument("interval",type=int, help="The number of times to pull data per
+            day. 1 or an even number between 2 and 24.")
+    parser.add_argument("listing_url", type=Path, help='The ebay listing url.')
+    return parser
+
+parser = argparse.ArgumentParser()
+
+if __name__ == "__main__":
+    arg_parser = create_parser()
+    parsed_args = arg_parser.parse_args(sys.argv[1:])
+    print(f"{parsed_args = }")
