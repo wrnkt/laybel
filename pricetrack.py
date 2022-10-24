4e8705948cb1fbe197538efe78af7110e89bead1
Niko <wrnkt1@gmail.com>
2022-10-24 04:20:19 -0400

    Add log_info decorator function for debugging

--- pricetrack.py
+++ pricetrack.py
@@ -3,6 +3,17 @@ import sys
 import argparse
 
 
+def log_info(debugmode:bool = False):
+    def outer_wrapper(func):
+        def wrapper(*args, **kwargs):
+            if (debugmode):
+                print(f"[LOG] calling {func.__name__}")
+            res = func(*args, **kwargs)
+            return res
+        return wrapper
+    return outer_wrapper
+
+@log_info(debugmode=True)
 def create_parser():
     parser = argparse.ArgumentParser(description='')
     parser.add_argument(
@@ -14,7 +25,6 @@ def create_parser():
     parser.add_argument("listing_url", type=str, help='The ebay listing url.')
     return parser
 
-parser = argparse.ArgumentParser()
 
 if __name__ == "__main__":
 
@@ -25,5 +35,9 @@ if __name__ == "__main__":
 
     if parsed_args.interval:
         interval = parsed_args.interval
+
+    url = parsed_args.listing_url
+
+    
 
 
