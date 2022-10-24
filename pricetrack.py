e9c7319d6a957d8673dacc75a02da0518747cf21
Niko <wrnkt1@gmail.com>
2022-10-24 01:14:17 -0400

    Handle interval set

--- pricetrack.py
+++ pricetrack.py
@@ -9,7 +9,7 @@ def create_parser():
             "-i",
             "--interval", 
             type=int,
-            help="""The number of times to pull data per day.  1 or an even
+            help="""The number of times to pull data per day. 1 or an even
             number between 2 and 24.""")
     parser.add_argument("listing_url", type=str, help='The ebay listing url.')
     return parser
@@ -17,6 +17,13 @@ def create_parser():
 parser = argparse.ArgumentParser()
 
 if __name__ == "__main__":
+
     arg_parser = create_parser()
     parsed_args = arg_parser.parse_args(sys.argv[1:])
-    print(f"{parsed_args = }")
+
+    interval = 1
+
+    if parsed_args.interval:
+        interval = parsed_args.interval
+
+
