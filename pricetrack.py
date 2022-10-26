1ecddcf5e149eae63757f15bcf8406e325bc43b3
Niko <wrnkt1@gmail.com>
2022-10-26 19:15:42 -0400

    Add get_sleep_delay function

--- pricetrack.py
+++ pricetrack.py
@@ -3,6 +3,7 @@ import sys
 import argparse
 import cProfile
 import pstats
+from threading import Timer
 
 def time_me(func):
     def wrapper(*args, **kwargs):
@@ -26,8 +27,8 @@ def log_info(debugmode:bool = False):
         return wrapper
     return outer_wrapper
 
-@time_me
-@log_info(debugmode=True)
+# @time_me
+# @log_info(debugmode=True)
 def create_parser():
     parser = argparse.ArgumentParser(description='')
     parser.add_argument(
@@ -38,6 +39,22 @@ def create_parser():
             number between 2 and 24.""")
     parser.add_argument("listing_url", type=str, help='The ebay listing url.')
     return parser
+
+def get_sleep_delay(interval: int) -> int:
+    seconds_in_day = 86400
+    second_delay = 0
+    if interval == 1:
+       second_delay = seconds_in_day
+    elif (1 < interval < 24) and (interval % 2 == 0):
+        second_delay = seconds_in_day // interval 
+    else:
+        raise NotImplementedError("Interval has to be 1 or an even number between 2 and 24.")
+
+    return second_delay
+
+
+def log_price(interval: int):
+     pass
 
 
 if __name__ == "__main__":
@@ -51,6 +68,9 @@ if __name__ == "__main__":
         interval = parsed_args.interval
 
     url = parsed_args.listing_url
+
+    print(f"{url = } {interval = }")
+    print(get_sleep_delay(2))
 
     
 
