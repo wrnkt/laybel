e69944b4ef3db6d1acade9a86029f176aac623a7
Niko <wrnkt1@gmail.com>
2022-12-09 14:06:09 -0500

    Get HTMLUnit working

--- src/main/java/com/tanchee/ListingRetriever.java
+++ src/main/java/com/tanchee/ListingRetriever.java
@@ -1,9 +1,18 @@
 package com.tanchee;
 
+import java.io.IOException;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
 public class ListingRetriever
 { 
-    public EbayListing getListing(String url)
+    public EbayListing getListing(String url) throws IOException
     {
+        try(WebClient webClient = new WebClient())
+        {
+            HtmlPage page = webClient.getPage(url);
+        }
+
         return EbayListing.test();
     }
 }
